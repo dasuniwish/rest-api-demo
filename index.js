3b45014e50701dd12c817a1a52a69489696143d3
dasuniwish <dasuniwishwani99@gmail.com>
2025-06-26 08:26:12 +0530

    Changes

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use('/api/products', productRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
